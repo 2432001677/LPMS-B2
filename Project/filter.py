9ac1014c144fe43159bf48845514a4ed66a01d7d
2432001677 <2432001677@qq.com>
2019-09-04 20:38:03 +0800

    添加注释

--- Project/filter.py
+++ Project/filter.py
@@ -5,12 +5,12 @@ def get_normal_cut_fs(freq_cut, freq):  # 归一化截止频率
     return 2 * freq_cut / freq
 
 
-def butter_low_pass(freq_cut, freq, order=5):
+def butter_low_pass(freq_cut, freq, order=5):  # 低通滤波，去除高于某一阈值频率的信号
     normal_freq_cut = get_normal_cut_fs(freq_cut, freq)
     return butter(order, normal_freq_cut, btype="lowpass")
 
 
-def butter_band(freqs, freq, order=5):
+def butter_band(freqs, freq, order=5):  # 带通滤波，保留中间频率信号
     normal_freq_cut = [get_normal_cut_fs(freqs[0], freq), get_normal_cut_fs(freqs[1], freq)]
     return butter(order, normal_freq_cut, btype="bandpass")
 
