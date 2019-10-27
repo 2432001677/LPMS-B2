f35fad5c64f107ca258fda435e46b334936a4857
王嵩 <31701018@stu.zucc.edu.cn>
2019-10-27 17:21:28 +0800

    update run,swim

--- Project/run.py
+++ Project/run.py
@@ -11,11 +11,11 @@ def main():
     order = 6  # 滤波器阶数
     fs = 400.0  # 采样率 hz
     freq_cut = 15  # 截止频率
-    filter = Filter(freq_cut, fs)  # 新建一个滤波器
+    filter = Filter(freq_cut, fs) # 新建一个滤波器
     b, a = filter.butter_low_pass(order)  # 生成一个分子b,分母a的低通滤波器
     # b, a = butter_band([10, 100], fs, order)
 
-    file = "butterfly1.csv"
+    file = "freestyle1.csv"
     data = Data(file)  # 读文件
     data_x = data.get_gyro()[0]
     data_y = data.get_gyro()[1]
@@ -23,37 +23,92 @@ def main():
     # y = lfilter(b, a, data)  # 有偏移量
     y = [filtfilt(b, a, data_x), filtfilt(b, a, data_y), filtfilt(b, a, data_z)]  # 无偏移量的处理后的数据列表
 
-    # generate_images(y, freq_cut, order,file)
+    #generate_images(y, freq_cut, order,file)
     # data_sta(y)
 
     # butterfly=Swim()
     # butterfly.gyro=y
-
-    start_time = 30000
-    end_time = 40000
-    # print(np.min(y[0][start_time:end_time]), end=' ')
-    # print(np.argmin(y[0][start:end_add]))
-    # print(np.max(y[1][start:end_add]), end=' ')
-    # print(np.argmax(y[1][start:end_add]))
-    # print(np.max(y[2][start:end_add]), end=' ')
-    # print(np.argmax(y[2][start:end_add]))
-
-    limit = (-1500, 2000)
+    # print(np.min(y[0][23700:23900]),end=' ')
+    # print(np.argmin(y[0][23700:23900])+23700)
+    # print(np.max(y[1][23700:23900]),end=' ')
+    # print(np.argmax(y[1][23700:23900])+23700)
+    # print(np.max(y[2][23700:23900]),end=' ')
+    # print(np.argmax(y[2][23700:23900])+23700)
+    start_time=0
+    end_time=len(y[2])
+    #limit=(-1500,2000)
     plt.subplot(311)
     plt.plot(y[0][start_time:end_time])
-    plt.ylim(limit)
+    #plt.ylim(limit)
     plt.grid()
+    plt.title("freestyle1")
     plt.subplot(312)
     plt.plot(y[1][start_time:end_time])
-    plt.ylim(limit)
+    #plt.ylim(limit)
     plt.grid()
     plt.subplot(313)
     plt.plot(y[2][start_time:end_time])
-    plt.ylim(limit)
+    #plt.ylim(limit)
     plt.grid()
-    plt.show()
-    s = Swim(y[2], start_time, end_time)
 
+    # plt.savefig("../1", dpi=1000)
+    plt.show()
+    s = Swim()
+    #     flg = 5000
+    # free=0
+    # butter=0
+    # back=0
+    # breast=0
+    # while start_time <= end_time-flg:
+    #     s.time_point_list.clear()
+    #     s.get_frequency(y[2], start_time, start_time+flg)
+    #     name = s.tst()
+    #     if name == "freestyle":
+    #         free += 1
+    #     if name == "butterfly":
+    #         butter += 1
+    #     if name == "backstroke":
+    #         back += 1
+    #     if name == "breaststroke":
+    #         breast += 1
+    #     start_time+=flg
+    # if free>butter and free>butter and free>breast:
+    #     s.name="freestyle"
+    #     s.time_frame_interval = 500  # 时间帧覆盖窗口大小
+    #     s.flag = 1000
+    #     s.flag2 = 500
+    #
+    #     list_z =s.get_frequency(y[2], start_time, end_time)
+    # if butter > free and butter > breast and butter > back:
+    #     s.name = "butterfly"
+    #     s.time_frame_interval = 500  # 时间帧覆盖窗口大小
+    #     s.flag = 1000
+    #     s.flag2 = 500
+    #
+    #     list_z =s.get_frequency(y[2], start_time, end_time)
+    # if back > butter and back > free and back > breast:
+    #     s.name = "backstroke"
+    #     s.time_frame_interval = 600  # 时间帧覆盖窗口大小
+    #     s.flag = 700
+    #     s.flag2 = 400
+    #
+    #     list_z =s.get_frequency(y[2], start_time, end_time)
+    # if breast > butter and breast > free and breast > back:
+    #     s.name = "breaststroke"
+    #     s.time_frame_interval = 600  # 时间帧覆盖窗口大小
+    #     s.flag = 400
+    #     s.flag2 = 300
+    #
+    #     list_z=s.get_frequency(y[2], start_time, end_time)
+    list_z=s.get_frequency(y[2], start_time, end_time)
+    name = s.tst()
+    s.print_inf()
+    # print(free,butter,back,breast)
+    print(list_z)
+    # print(np.ptp(y[2][26000:34000]))
+    # print(np.amax(y[2][26000:34000]))
+    # for i in list_z:
+    #     print(y[2][i])
 
 if __name__ == '__main__':
     main()
--- Project/swim.py
+++ Project/swim.py
@@ -2,40 +2,106 @@ from Project import np
 
 
 class Swim:
-    def __init__(self, y, start_time, end_time):
-        self.acc = self.gyro = self.mag = self.quat = None
-        self.sum_distance = 0  # 总距离
-        self.get_frequency(y, start_time, end_time)
+    acc = gyro = mag = quat = None
+    sum_distance = 0  # 总距离
+    number=0
+    all_time=0
+    arm_stroke=1.5
+    once_time=0
+    pool=0
+    time_frame_interval = 500  # 时间帧覆盖窗口大小
+    flag = 1000
+    flag2 = 500
+    name=None
+    time_point_list = []
+    example=[]
+    start_time=0
+    end_time=0
 
-    def get_frequency(self, y, start_time, end_time):
-        max_time_interval = 1000
+    # free/buffer 500 1000 500
+    #back 600 700 400
+    # breast 600 400 220 有误差，无法对比
+
+    def get_frequency(self, y,start_time, end_time):
+        self.example=y
+        self.start_time=start_time
+        self.end_time=end_time
+        max_time_interval = 1000;
         all_time = 0
-        arm_stroke = 1.5
-        time_frame_interval = 500  # 时间帧覆盖窗口大小
+        arm_stroke = self.arm_stroke
+
+        time_frame_interval=self.time_frame_interval
         increment_time_frame = 25  # 时间帧向前滑动距离
-        flag = 900
-        self.sum_distance = end_time - start_time
-        time_point_list = []
+        flag = self.flag
+        flag2=self.flag2
+        sum_distance = end_time-start_time
+        time_point_list = self.time_point_list
         while start_time < end_time - time_frame_interval:
             x = np.ptp(y[start_time:start_time + time_frame_interval])
             if x > flag:
-                max_value = np.amax(y[start_time:start_time + time_frame_interval])
+                max_value=np.amax(y[start_time:start_time + time_frame_interval])
                 for i in range(time_frame_interval):
-                    if y[start_time + i] == max_value and y[start_time + i] > 500:
-                        time_point_list.append(start_time + i)
+                    if y[start_time+i]==max_value and y[start_time+i]>flag2:
+                        time_point_list.append(start_time+i)
 
                 start_time += time_frame_interval
-                while y[start_time] > 500:
+                while y[start_time] > flag2:
                     start_time += increment_time_frame
             else:
                 start_time += increment_time_frame
 
-        self.all_time = time_point_list[len(time_point_list) - 1] - time_point_list[0]  # 总时间
-        self.number = len(time_point_list)  # 划臂次数
-        self.once_time = all_time / self.number  # 单词划臂时间
+        if len(time_point_list)>1:
+            self.all_time = time_point_list[len(time_point_list)-1]-time_point_list[0]
+            self.number = len(time_point_list)
+            self.once_time = self.all_time / self.number
         # 计算所有时间间隔，取平均
-        self.pool = self.number * arm_stroke  # 游泳距离
-        for i in range(len(time_point_list)):
-            if time_point_list[i + 1] - time_point_list[i] > 2 * self.once_time:
-                print(i)  # 转向处
-                break
+        #    self.pool = self.number * self.arm_stroke
+
+
+        # for i in range(len(time_point_list)):
+        #     if time_point_list[i+1]-time_point_list[i]>2*once_time:
+        #         print(i)#转向处
+        #         break
+        #self.time_point_list.clear()
+        self.time_point_list=time_point_list
+
+        return time_point_list
+
+    def tst(self):
+
+        if len(self.time_point_list)>5 :
+            if np.amax(self.example[self.time_point_list[0]:self.time_point_list[len(self.time_point_list)-1]])>1000:
+                self.name="butterfly"
+            else:
+                if np.amax(self.example[
+                           self.time_point_list[0]:self.time_point_list[len(self.time_point_list) - 1]]) > 500:
+                    self.name="freestyle"
+        else:
+            self.time_frame_interval = 600  # 时间帧覆盖窗口大小
+            self.flag = 700
+            self.flag2 = 400
+            self.time_point_list.clear()
+            self.get_frequency(self.example,self.start_time,self.end_time)
+            if len(self.time_point_list) > 5:
+                if np.amax(self.example[
+                           self.time_point_list[0]:self.time_point_list[len(self.time_point_list) - 1]]) > 500:
+                    self.name = "backstroke"
+            else:
+                self.time_frame_interval = 600  # 时间帧覆盖窗口大小
+                self.flag = 400
+                self.flag2 = 300
+                self.time_point_list.clear()
+                self.get_frequency(self.example, self.start_time, self.end_time)
+                if len(self.time_point_list) > 1:
+                    if np.amax(self.example[
+                                self.time_point_list[0]:self.time_point_list[len(self.time_point_list) - 1]]) < 700:
+                         self.name = "breaststroke"
+
+        return self.name
+
+    def print_inf(self):
+        print(self.number)  # 划臂次数
+        print(self.all_time)  # 总时间
+        print(self.once_time)  # 单词划臂时间
+        print(self.pool)  # 游泳距离
+        print(self.name)
